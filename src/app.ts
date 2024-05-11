2da68289e021bb903d5f5edfc46bb21400ae8300
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-10 23:23:03 -0600

    added auth routes middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Express, Response } from 'express';
+import authRoutes from './routes/authRoutes';
 
 const app: Express = express();
 const port = process.env.PORT || 5050;
@@ -7,5 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => res.send('Ooooh Yea!'));
+
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => console.log(`Server running oooh so smoothly at http://localhost:${port}`));
