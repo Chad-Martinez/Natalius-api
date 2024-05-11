d6342caec2fe691a68b9eac51e1c32580a41bad7
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-11 12:41:07 -0600

    added db connection and error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
-import express, { Request, Express, Response } from 'express';
+import express, { Request, Express, Response, NextFunction, urlencoded, json } from 'express';
+import HttpErrorResponse from './classes/HttpErrorResponse';
+import dbConnect from './db/conn';
 import authRoutes from './routes/authRoutes';
+
+dbConnect();
 
 const app: Express = express();
 const port = process.env.PORT || 5050;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => res.send('Ooooh Yea!'));
 
 app.use('/api/auth', authRoutes);
+
+// eslint-disable-next-line no-unused-vars
+app.use((error: Error, req: Request, res: Response, next: NextFunction): void => {
+  if (error instanceof HttpErrorResponse) {
+    res.status(error.status).json({ message: error.message });
+  }
+  console.error('Express Error Middleware: ', error);
+  res.status(500).json({ message: 'Internal Server Error', error });
+});
 
 app.listen(port, () => console.log(`Server running oooh so smoothly at http://localhost:${port}`));
