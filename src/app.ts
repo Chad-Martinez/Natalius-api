d2bcd681349c8a51792e8826223c0bf0959ba511
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-10 23:22:39 -0600

    added body parser and urlencoded middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import express, { Request, Express, Response } from 'express';
 const app: Express = express();
 const port = process.env.PORT || 5050;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req: Request, res: Response) => res.send('Ooooh Yea!'));
 
 app.listen(port, () => console.log(`Server running oooh so smoothly at http://localhost:${port}`));
