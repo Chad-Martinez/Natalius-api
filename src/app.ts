b2d1bfa5964c298b081cfb2deb1aee8b1c2b8cbc
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-26 01:40:57 -0600

    set root route to check deploy status

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Natalius API is active' });
+  res.status(200).send('Natalius API is active').json({ message: 'Natalius API is active' });
 });
 
 app.use('/api/auth', authRoutes);
