51fa0da51e12f36109a9e2f81b5cd3e49ce8352a
chadcito <145295470+chadcito@users.noreply.github.com>
2024-10-07 23:02:06 -0600

    added IShiftExpenses and IShiftIncome and shiftComplete to Shift interface and model.

--- src/interfaces/Shift.interface.ts
+++ src/interfaces/Shift.interface.ts
@@ -1,14 +1,37 @@
 import { Types } from 'mongoose';
+import { EnumType } from 'typescript';
+
+export interface IShiftExpenses {
+  floorFee: Number;
+  dances: {
+    numOfDances: Number;
+    pricePerDance: Number;
+    danceFeeTotal: Number;
+  };
+  tips: Number;
+  other: Number;
+  totalShiftExpenses: Number;
+  type: String;
+}
+
+export interface IShiftIncome {
+  amount: Number;
+  type: EnumType;
+}
 
 export interface IShiftBase {
   start: Date;
   end: Date;
-  notes?: String;
-  incomeReported?: boolean;
+  notes: String;
+  shiftComplete: boolean;
   clubId: Types.ObjectId;
+  expenses: IShiftExpenses;
+  income: IShiftIncome;
+  milage: Number;
   userId: Types.ObjectId;
 }
 
 export interface IShift extends IShiftBase {
   _id: Types.ObjectId;
+  clubName?: String;
 }
