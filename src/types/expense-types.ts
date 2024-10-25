41b07b226af000322cf82307187c4ea6186954b3
chadcito <145295470+chadcito@users.noreply.github.com>
2024-10-24 22:42:25 -0600

    added pie and graph expense types

--- src/types/expense-types.ts
+++ src/types/expense-types.ts
@@ -5,3 +5,32 @@ export type ExpensesByTypeAndPeriod = {
   type: string;
   totalAmount: number;
 };
+
+type PieDataSet = {
+  count: number;
+  label: string;
+  value: number;
+};
+
+export type PieData = {
+  month: PieDataSet[];
+  quarter: PieDataSet[];
+  year: PieDataSet[];
+  defaultDataSet: string;
+};
+
+export type GraphType = {
+  type: string;
+  totalExpense: number;
+};
+
+export type GraphCurrentPeriod = {
+  month: string;
+  types: GraphType[];
+};
+
+export type GraphSet = {
+  weeklyExpenseCurrentMonth: GraphCurrentPeriod[];
+  monthlyExpenseCurrentQuarter: GraphCurrentPeriod[];
+  monthlyExpenseCurrentYear: GraphCurrentPeriod[];
+};
