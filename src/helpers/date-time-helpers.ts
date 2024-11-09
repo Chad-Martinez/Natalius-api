2197198d9cc23abc7aa1e58201b9adc4ccc88acc
Chad Martinez <cmartinez@blackburnlabs.com>
2024-11-08 21:21:52 -0700

    added date name helpers

---
+++ src/helpers/date-time-helpers.ts
@@ -0,0 +1,3 @@
+export const DAYS_OF_WEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+export const MONTHS_OF_YEAR = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
