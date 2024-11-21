0513fc9e8f95c8cd9eae6dc6ed419a2d427ced98
chadcito <145295470+chadcito@users.noreply.github.com>
2024-11-21 03:18:03 -0700

    added build files

--- dist/routes/shiftRoutes.js
+++ dist/routes/shiftRoutes.js
@@ -4,6 +4,7 @@ const express_1 = require("express");
 const shiftController_1 = require("../controllers/shiftController");
 const router = (0, express_1.Router)();
 router.get('/club/:clubId', shiftController_1.getActiveShiftsByClub);
+router.get('/shifts-to-complete', shiftController_1.getShiftsToComplete);
 router.get('/:shiftId', shiftController_1.getShiftById);
 router.post('/', shiftController_1.addShift);
 router.put('/', shiftController_1.updateShift);
