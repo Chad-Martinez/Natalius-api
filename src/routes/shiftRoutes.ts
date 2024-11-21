1b17757e218596b8f359ee0bf082931736c4a308
chadcito <145295470+chadcito@users.noreply.github.com>
2024-11-21 03:16:51 -0700

    created shifts-to-complete route

--- src/routes/shiftRoutes.ts
+++ src/routes/shiftRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { addShift, deleteShift, getActiveShiftsByClub, getShiftById, updateShift } from '../controllers/shiftController';
+import { addShift, deleteShift, getActiveShiftsByClub, getShiftById, getShiftsToComplete, updateShift } from '../controllers/shiftController';
 
 const router = Router();
 
 router.get('/club/:clubId', getActiveShiftsByClub);
+
+router.get('/shifts-to-complete', getShiftsToComplete);
 
 router.get('/:shiftId', getShiftById);
 
